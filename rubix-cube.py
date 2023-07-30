0446273d2a3927e3d736d53fda7beb62c17e49f7
penguinerd1000 <jovanson.zach@gmail.com>
2023-07-29 21:11:03 -0700

    added rubix-cube.py

---
+++ rubix-cube.py
@@ -0,0 +1,39 @@
+#abbreviation for ease of typing
+w, g, r ,b ,o, y = "white", "green", "red", "blue", "orange", "yellow"
+
+#assigned at starting position
+whiteface = [
+    [w, w, w], 
+    [w, w, w] 
+    [w, w, w]
+]
+
+greenface = [
+    [g, g, g],
+    [g, g, g],
+    [g, g, g],
+]
+
+redface = [
+    [r, r, r],
+    [r, r, r],
+    [r, r, r],
+]
+
+blueface = [
+    [b, b, b],
+    [b, b, b],
+    [b, b, b],
+]
+
+orangeface = [
+    [o, o, o],
+    [o, o, o],
+    [o, o, o],
+]
+
+yellowface = [
+    [y, y, y], 
+    [y, y, y], 
+    [y, y, y], 
+]
